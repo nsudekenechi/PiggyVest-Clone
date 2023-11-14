081bd99ba2c3ad12f73bd3c6ae263dd06ba85607
Kenechi <nsudekenechi2@gmail.com>
2023-11-15 00:24:44 +0100

    Added Loading to investify route

---
+++ app/dashboard/investify/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <div className='h-[300px] flex items-center justify-center'>
+            
+            <span className="loading loading-dots loading-lg gap-x-10"></span>
+        </div>
+    )
+}
+
+export default loading
