c4f9cb34fa8556e732ae6551b575802e699e218a
Kenechi <nsudekenechi2@gmail.com>
2023-11-17 03:22:29 +0100

    Added Exit Animation when modal is closing

--- app/dashboard/@sidebar/sidebar/Modal.jsx
+++ app/dashboard/@sidebar/sidebar/Modal.jsx
@@ -4,14 +4,21 @@ import { IoIosClose } from 'react-icons/io'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
-const Modal = ({ children,color }) => {
-    const router = useRouter()
+const Modal = ({ children, color }) => {
+    const router = useRouter();
+    const [showModal,setShowModal] = useState(true);
+    const goBack = () => {
+        setTimeout(()=>{
+            router.back()
+        },500)
+        setShowModal(false);
+    }
     return (
         <div className='fixed  w-[100%] h-[100%] z-[100] grid grid-cols-6 bg-[rgba(0,0,0,.4)]' >
-            <div className="col-span-4 " onClick={()=> router.back()}></div>
-            <motion.div initial={{translateX:"50%"}} animate={{translateX:"0%"}} transition={{duration: 0.4}} className='bg-white col-span-2 '>
+            <div className="col-span-4 " onClick={goBack}></div>
+            <motion.div initial={{ translateX: "50%" }} animate={{ translateX: showModal ? "0%" : "100%" }} transition={{ duration: 0.4 }} className='bg-white col-span-2 '>
                 <div className={`flex justify-end mb-10 ${color} p-5`} >
-                    <IoIosClose size={50} className='cursor-pointer'  onClick={()=> router.back()}/>
+                    <IoIosClose size={50} className='cursor-pointer' onClick={goBack} />
                 </div>
                 {children}
             </motion.div>
